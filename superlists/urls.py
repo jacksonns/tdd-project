21d697430c25a7afe9a31a71212c27b25e6ac39b
jacksonns <jackson.guima2@gmail.com>
2022-06-14 19:59:11 -0300

    Testando o envio de email: redirecionando para o home

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.urls import re_path
 from lists import views
+from accounts import views as accounts_views
 
 urlpatterns = [
     re_path(r'^$', views.home_page, name='home'),
     re_path(r'^lists/new$', views.new_list, name='new_list'),
     re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^accounts/send_login_email$', accounts_views.send_login_email, name='send_login_email'),
 ]
